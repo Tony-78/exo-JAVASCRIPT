3ce320d1ead4fe046d11a06c0810b63344362fb2
Tony <tonyprou@gmail.com>
2020-11-10 12:36:47 +0100

    ajout else dans la condition

--- exo 6 - boucles/Exercice 2/script.js
+++ exo 6 - boucles/Exercice 2/script.js
@@ -1,10 +1,11 @@
 for (number = 0; number <= 100 ; number++) {
     let multiple = document.createElement("p");
-    multiple.textContent = number ;
-    document.body.append(multiple);
     if (number % 15 == 0){
         multiple.textContent = "...";
+    } else {
+        multiple.textContent = number ;
     }
+    document.body.append(multiple);
 }
 
 // A l'aide d'une boucle afficher tous les nombre de 0 à 100. Pour chaque multiples de 15, les remplacer par '...'
