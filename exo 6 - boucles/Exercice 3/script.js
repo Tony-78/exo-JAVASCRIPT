e54099f4ddd53cb9301d8ce983dab676d42bc89a
Tony <tonyprou@gmail.com>
2020-11-10 12:35:27 +0100

    ajout boucle for each

--- exo 6 - boucles/Exercice 3/script.js
+++ exo 6 - boucles/Exercice 3/script.js
@@ -1,8 +1,21 @@
 let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
-for (i = 0 ; i < languages.length ; i++) {
+
+
+// FOR 
+
+for (nameLanguage = 0 ; nameLanguage < languages.length ; nameLanguage++) {
     let text = document.createElement("p");
-    text.textContent = languages[i] ;
+    text.textContent = languages[nameLanguage] ;
     document.body.append(text);
 }
 
 // Dans le fichier html fourni, afficher tous les éléments du tableau languages.
+
+
+// FOR EACH
+
+// languages.forEach(element => {
+//     let text = document.createElement("p");
+//     text.textContent = element;
+//     document.body.append(text);
+// });
--- exo 6 - boucles/Exercice 4/script.js
+++ exo 6 - boucles/Exercice 4/script.js
@@ -1,4 +1,9 @@
 let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi','Dimanche'];
+
+
+
+// FOR 
+
 for ( i = 0 ; i < days.length ; i++) {
     let text = document.createElement("p");
     text.textContent = days[i] ;
@@ -7,5 +12,19 @@ for ( i = 0 ; i < days.length ; i++) {
     text.style.fontWeight = "bold";
 }
 }
+
+
+// FOR EACH
+
+// days.forEach(element => {
+//     let text = document.createElement("p");
+//     text.textContent = element;
+//     document.body.append(text);
+//     if ((element == "Samedi" || element == "Dimanche")){
+//         text.style.fontWeight = "bold";
+//     }
+// });
+
+
 
 // Dans un fichier html fourni, afficher tous les éléments du tableau days. Mettre en gras le samedi et le dimanche.
